6915c246deb91cf4e00ae76b7d6dffd60e2aab91
alexcactus0 <217364473+alexcactus0@users.noreply.github.com>
2025-08-19 18:58:39 +0200

    -designed the shape

---
+++ js-file.js
@@ -0,0 +1,23 @@
+const add = () => {
+    
+};
+
+const subtract = () => {
+
+};
+
+const multiply = () => {
+
+};
+
+const divide = () => {
+
+};
+
+let number = 3;
+let operator = "+";
+let secnumber = 5;
+
+const operate = () => {
+
+};
