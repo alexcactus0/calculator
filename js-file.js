3113e7405a06ccaa9d28935e8effd7828a8ed084
alexcactus0 <217364473+alexcactus0@users.noreply.github.com>
2025-08-20 23:32:17 +0200

    -Finished the whole project, as fun and frustrating as it was I'm ready for more.

--- js-file.js
+++ js-file.js
@@ -1,23 +1,83 @@
-const add = () => {
-    
+// operator function
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => b !== 0 ? a / b : "ERROR!";
+
+const operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 };
 
-const subtract = () => {
-
+const operate = (operator, a, b) => {
+    return operations[operator](a, b);
 };
 
-const multiply = () => {
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = null;
+let displayValue = "";
 
-};
+// displaying digits
+const digitButtons = document.querySelectorAll(".digit");
+const output = document.querySelector(".output");
 
-const divide = () => {
+    digitButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            displayValue += button.textContent;
+            output.textContent = displayValue;
 
-};
+            if (currentOperator === null) {
+                firstNumber = displayValue;
+            } else {
+                secondNumber = displayValue;
+            }
+        });
+    });
 
-let number = 3;
-let operator = "+";
-let secnumber = 5;
+// operating buttons
+const operatorButtons = document.querySelectorAll(".operator");
 
-const operate = () => {
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        let symbol = button.textContent;
 
-};
+        if (symbol === "x") symbol = "*";
+        if (symbol === "÷") symbol = "/";
+
+        currentOperator = symbol;
+        displayValue = "";
+        output.textContent = "";
+    });
+});
+
+// equal button
+const equalButton = document.querySelector(".solution");
+
+equalButton.addEventListener("click", () => {
+    if (firstNumber !== "" && secondNumber !== "" && currentOperator !== null) {
+        const result = operate(currentOperator, Number(firstNumber), Number(secondNumber));
+        output.textContent = result;
+
+        firstNumber = result;
+        secondNumber = "";
+        currentOperator = null;
+        displayValue = "";
+    };
+});
+
+//clear button
+const clearAll = document.querySelector(".delete");
+
+    clearAll.addEventListener("click", () => {
+        firstNumber = "";
+        secondNumber = "";
+        currentOperator = null;
+        displayValue = "";
+        output.textContent = "";
+    });
